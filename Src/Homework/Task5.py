39558fa48d270a0046bae587ace56583aecd6d32
vikash Rawat <vikash.rawat1090@gmail.com>
2024-08-19 03:09:58 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

---
+++ Src/Homework/Task5.py
@@ -0,0 +1,11 @@
+#WAP to compare two number if they are greater,smaller or eqaual to each other
+
+
+num1=int(input("Enter the first Number "))
+num2=int(input("Enter the Second Number "))
+
+print("Number1 is greater than number2:",num1>num2)
+print("Number1 is less than number2:",num1<num2)
+print("Number1 is equal than number2:",num1==num2)
+
+
