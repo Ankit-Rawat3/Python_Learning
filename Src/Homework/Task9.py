e6a0da43b62da36d1856d14e57c2a9469eb0836b
vikash Rawat <vikash.rawat1090@gmail.com>
2024-08-20 17:48:20 +0530

    Adding new files

--- Src/Homework/Task9.py
+++ Src/Homework/Task9.py
@@ -4,10 +4,10 @@ However, for multiples of 3, print "Fizz" instead of the number, and
 for multiples of 5, print "Buzz." 
 For numbers that are multiples of both 3 and 5, print 'FizzBuzz'."""""
 
-for i in range(1,101):
-    if(i%3==0) and i%5==0:
+for i in range(1, 101):
+    if (i % 3 == 0) and i % 5 == 0:
         print(f"{i} : FizzBuzz")
-    elif i%5==0:
+    elif i % 5 == 0:
         print(f"{i} : Buzz")
-    elif i%3==0:
+    elif i % 3 == 0:
         print(f"{i} : Fizz")
