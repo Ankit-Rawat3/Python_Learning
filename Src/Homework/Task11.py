d35fc6af9e02016630a7f66da4b85165c6bfb2a6
vikash Rawat <vikash.rawat1090@gmail.com>
2024-08-24 00:06:33 +0530

    Adding new files

--- Src/Homework/Task11.py
+++ Src/Homework/Task11.py
@@ -4,7 +4,7 @@
 num=int(input("Enter a number to create Fibonacci series : \n"))
 series=[]
 a,b=0,1
-for i in range(num+1):
+for i in range(2,num+1):
     series.append(a)
     a, b = b, a + b
 
--- Src/Learning_22082024/Lab051.py
+++ Src/Learning_22082024/Lab051.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
+"""def fibonacci(n):
     a, b = 0, 1
     for _ in range(n):
         print(a)
@@ -6,3 +6,18 @@ def fibonacci(n):
 
 # Example: Print the first 10 Fibonacci numbers
 fibonacci(7)
+"""
+
+"or"
+
+num_terms = int(input("Enter the number of terms in the Fibonacci series: "))
+a,b=0,1
+
+print("fabonacci series")
+count=0
+while count<=num_terms:
+    print(a, end= ' ')
+
+    a,b=b,a+b
+
+    count=count+1
