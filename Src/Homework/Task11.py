8a25d4fd71614ad43468e8849dabbb704d2b361c
Ankit Rawat <nktrwt04@gmail.com>
2024-08-24 00:16:42 +0530

    Adding new files

--- Src/Homework/Task11.py
+++ Src/Homework/Task11.py
@@ -4,11 +4,11 @@
 num=int(input("Enter a number to create Fibonacci series : \n"))
 series=[]
 a,b=0,1
-for i in range(2,num+1):
+for i in range(num+1):
     series.append(a)
     a, b = b, a + b
 
-print(f"Fibonaci series for number {num} is {series}")
+print(f"Fibonacci series for number {num} is {series}")
 
 
 
