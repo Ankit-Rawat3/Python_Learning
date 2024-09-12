afb5459ff9544316d7967da16b1c3b578f122fcc
Ankit Rawat <nktrwt04@gmail.com>
2024-09-12 08:20:05 +0530

    Adding new files - 12 Sept

---
+++ Src/Sep 2024/Learning 12092024/Lab148.py
@@ -0,0 +1,12 @@
+#requests
+#webdriver (selenium 4)
+#csv,os
+#pytest,json,allure report,xml,playwright,openpyxml,openpycsv,
+
+#pip - package manager - help to import other packages into your project
+
+#Automation Project
+#Selenium,request,csv,os,pytest,json,playwright
+
+
+
